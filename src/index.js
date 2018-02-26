8ddb30abeec99e28acec1498f8d4a38ffba1c01c
Adrian Apostol <adrian.apostol@macys.com>
2018-02-26 18:34:37 +0200

    a

--- src/index.js
+++ src/index.js
@@ -18,10 +18,11 @@ d.buildDictionary(__dirname + '/dictionary.txt').then( a => {
     var trainer = new Trainer(lstm);
 
     var info = trainer.train(trainingSet, {
-            iterations: 100000,
-            log: 10000,
-            rate: .0012,
-            error: .01
+            iterations: 1000000,
+            log: 1000,
+            rate: .0005,
+            error: .005,
+            cost: ccost
         }
     );
 
@@ -39,27 +40,10 @@ d.buildDictionary(__dirname + '/dictionary.txt').then( a => {
 
 });
 
+function ccost(target, output) {
+    var mse = 0;
+    for (var i = 0; i < output.length; i++)
+      mse += Math.abs(target[i] - output[i]);
+    return mse / output.length;
+  }
 
-// iterations 10000 error 0.06549594736345705 rate 0.03
-// iterations 20000 error 0.06546996596133348 rate 0.03
-// iterations 30000 error 0.06546882328849055 rate 0.03
-// iterations 40000 error 0.06546506375928421 rate 0.03
-// iterations 50000 error 0.0654614573461793 rate 0.03
-
-// iterations 10000 error 0.062259850033969374 rate 0.01
-// iterations 20000 error 0.059360057277992755 rate 0.01
-// iterations 30000 error 0.05725724933257616 rate 0.01
-// iterations 40000 error 0.055736975008530074 rate 0.01
-// iterations 50000 error 0.05627062824074254 rate 0.01
-
-// iterations 10000 error 0.05977595183546593 rate 0.005
-// iterations 20000 error 0.052231399297389576 rate 0.005
-// iterations 30000 error 0.046946950198550115 rate 0.005
-// iterations 40000 error 0.0351540510789493 rate 0.005
-// iterations 50000 error 0.05103779601143528 rate 0.005
-
-// iterations 10000 error 0.06230024435302879 rate 0.0025
-// iterations 20000 error 0.05542695152187594 rate 0.0025
-// iterations 30000 error 0.05443247811325467 rate 0.0025
-// iterations 40000 error 0.052670779682994356 rate 0.0025
-// iterations 50000 error 0.04944637180643104 rate 0.0025
